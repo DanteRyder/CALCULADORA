da1ff46c7c008c79239a66b687305f14a64d9971
Isaías França <isaias_leite_@outlook.com>
2022-02-11 22:54:54 -0300

    Calculadora Finalizada

--- index.js
+++ index.js
@@ -2,8 +2,31 @@ let display = document.getElementById('display');
 
 let buttons = Array.from(document.getElementsByClassName('button'));
 
-buttons.map( button => {
+buttons.map(button => {
     button.addEventListener('click', (e) => {
-        console.log("TESTANDO: " + e.target.innerText);
+        console.log('TESTANDO: ' + e.target.innerText);
+
+        // LOGICA ? VERDADEIRO : FALSO
+        display.innerText == 'ERROR' ? display.innerText = null : display.innerText;
+
+        switch (e.target.innerText) {
+            case 'C':
+                display.innerText = null;
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText);
+                }
+                catch {
+                    display.innerText = 'ERROR';
+                }
+                break;
+            case '←':
+                display.innerText = display.innerText.slice(0, -1);
+                break;
+            default:
+                display.innerText += e.target.innerText;
+                break;
+        }
     });
 });
